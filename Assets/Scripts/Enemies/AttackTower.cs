e451c225acc22d78990d4d9181ad0686c34f9fa0
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2025-08-24 14:07:39 +0200

    Ship Explosion works

--- Assets/Scripts/Enemies/AttackTower.cs
+++ Assets/Scripts/Enemies/AttackTower.cs
@@ -1,16 +1,18 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class AttackTower : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    
+
+    public GameObject Explosion;
 
     public void BlowUpTower(float Damage)
     {
 
         GameManagerEvent decreaseHealth = new GameManagerEvent(Damage, StatsChange.Health);
         EventBus.Act(decreaseHealth);
+        Instantiate(Explosion, this.gameObject.transform.position, quaternion.identity);
         Destroy(this.gameObject);
 
     }
